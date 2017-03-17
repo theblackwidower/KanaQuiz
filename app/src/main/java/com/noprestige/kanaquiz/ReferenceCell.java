b337fee46d18b99b3e62747cbcf2b4a4ca1f3120
T Duke Perry <theblackwidower@noprestige.com>
2017-03-17 07:47:05 -0400

    Built ReferenceCell class
    This class is to be used in the upcoming reference table feature. Each
    one will hold a single kana for display, alongside its romanji
    equivalent.

---
+++ app/src/main/java/com/noprestige/kanaquiz/ReferenceCell.java
@@ -0,0 +1,139 @@
+package com.noprestige.kanaquiz;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+public class ReferenceCell extends View
+{
+    private String kana;
+    private String romanji;
+    private float kanaSize;
+    private float romanjiSize;
+
+    private TextPaint kanaPaint = new TextPaint();
+    private TextPaint romanjiPaint = new TextPaint();
+
+    private float kanaXpoint;
+    private float kanaYpoint;
+    private float romanjiXpoint;
+    private float romanjiYpoint;
+
+    private float kanaWidth;
+    private float kanaHeight;
+    private float romanjiWidth;
+    private float romanjiHeight;
+
+    private float fullHeight;
+
+    public ReferenceCell(Context context)
+    {
+        super(context);
+        init(null, 0);
+    }
+
+    public ReferenceCell(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ReferenceCell(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle)
+    {
+        final TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.ReferenceCell, defStyle, 0);
+
+        Context context = this.getContext();
+
+        kana = a.getString(R.styleable.ReferenceCell_kana);
+        romanji = a.getString(R.styleable.ReferenceCell_romanji);
+        kanaSize = a.getDimension(R.styleable.ReferenceCell_kanaSize,
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 64, context.getResources().getDisplayMetrics()));
+        romanjiSize = a.getDimension(R.styleable.ReferenceCell_romanjiSize,
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, context.getResources().getDisplayMetrics()));
+
+        a.recycle();
+
+        updateObject();
+    }
+
+    private void updateObject()
+    {
+        kanaPaint.setTextSize(kanaSize);
+        romanjiPaint.setTextSize(romanjiSize);
+
+        kanaWidth = kanaPaint.measureText(kana);
+        romanjiWidth = romanjiPaint.measureText(romanji);
+        kanaHeight = kanaPaint.getFontMetrics().descent - kanaPaint.getFontMetrics().ascent;
+        romanjiHeight = romanjiPaint.getFontMetrics().descent - romanjiPaint.getFontMetrics().ascent;
+        fullHeight = kanaHeight + romanjiHeight;
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight)
+    {
+        super.onSizeChanged(width, height, oldWidth, oldHeight);
+
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = width - paddingLeft - paddingRight;
+        int contentHeight = height - paddingTop - paddingBottom;
+
+        kanaXpoint = paddingLeft + (contentWidth - kanaWidth) / 2;
+        kanaYpoint = paddingTop + (contentHeight - fullHeight) / 2 - kanaPaint.getFontMetrics().ascent;
+        romanjiXpoint = paddingLeft + (contentWidth - romanjiWidth) / 2;
+        romanjiYpoint = paddingTop + (contentHeight + fullHeight) / 2 - romanjiPaint.getFontMetrics().descent;
+    }
+
+    //ref: http://stackoverflow.com/questions/13273838/onmeasure-wrap-content-how-do-i-know-the-size-to-wrap
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        int desiredWidth = Math.round(Math.max(kanaWidth, romanjiWidth));
+        int desiredHeight = Math.round(fullHeight);
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int width;
+        int height;
+
+        if (widthMode == MeasureSpec.EXACTLY)
+            width = widthSize;
+        else if (widthMode == MeasureSpec.AT_MOST)
+            width = Math.min(desiredWidth, widthSize);
+        else
+            width = desiredWidth;
+
+        if (heightMode == MeasureSpec.EXACTLY)
+            height = heightSize;
+        else if (heightMode == MeasureSpec.AT_MOST)
+            height = Math.min(desiredHeight, heightSize);
+        else
+            height = desiredHeight;
+
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+
+        canvas.drawText(kana, kanaXpoint, kanaYpoint, kanaPaint);
+        canvas.drawText(romanji, romanjiXpoint, romanjiYpoint, romanjiPaint);
+    }
+}
