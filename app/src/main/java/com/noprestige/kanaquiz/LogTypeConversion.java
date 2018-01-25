b120ab1bd4b858aad8c51c9e081daf3230ef968f
T Duke Perry <theblackwidower@noprestige.com>
2018-01-24 20:32:19 -0500

    Modified LogTypeConversion
    By modifying the code in LogTypeConversion, the date is stored in a more
    simplified fashion, not storing the time, or the number of milliseconds,
    instead just storing a single integer representing nothing more than the
    year, month, and day.

--- app/src/main/java/com/noprestige/kanaquiz/LogTypeConversion.java
+++ app/src/main/java/com/noprestige/kanaquiz/LogTypeConversion.java
@@ -3,18 +3,21 @@ package com.noprestige.kanaquiz;
 import android.arch.persistence.room.TypeConverter;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class LogTypeConversion
 {
     @TypeConverter
-    public Date fromTimestamp(Long value)
+    public Date fromTimestamp(Integer value)
     {
-        return value == null ? null : new Date(value);
+        return value == null ? null :
+                new GregorianCalendar(value / 10000, value % 10000 / 100, value % 100).getTime();
     }
 
     @TypeConverter
-    public static Long dateToTimestamp(Date date)
+    public static Integer dateToTimestamp(Date date)
     {
-        return date == null ? null : date.getTime();
+        //TODO: replace deprecated methods
+        return date == null ? null : (date.getYear() + 1900) * 10000 + date.getMonth() * 100 + date.getDate();
     }
 }
