76b758b8a9b2334189899a755a1495a4657bc359
T Duke Perry <theblackwidower@noprestige.com>
2018-01-27 16:57:22 -0500

    Replaced deprecated methods
    Replaced all the deprecated methods in LogTypeConversion. Also changed
    the month to start at one and not zero in the database.

--- app/src/main/java/com/noprestige/kanaquiz/LogTypeConversion.java
+++ app/src/main/java/com/noprestige/kanaquiz/LogTypeConversion.java
@@ -5,19 +5,29 @@ import android.arch.persistence.room.TypeConverter;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.MONTH;
+import static java.util.Calendar.YEAR;
+
 public class LogTypeConversion
 {
     @TypeConverter
     public Date fromTimestamp(Integer value)
     {
         return value == null ? null :
-                new GregorianCalendar(value / 10000, value % 10000 / 100, value % 100).getTime();
+                new GregorianCalendar(value / 10000, value % 10000 / 100 - 1, value % 100).getTime();
     }
 
     @TypeConverter
     public static Integer dateToTimestamp(Date date)
     {
-        //TODO: replace deprecated methods
-        return date == null ? null : (date.getYear() + 1900) * 10000 + date.getMonth() * 100 + date.getDate();
+        if (date == null)
+            return null;
+        else
+        {
+            GregorianCalendar calendar = new GregorianCalendar();
+            calendar.setTime(date);
+            return calendar.get(YEAR) * 10000 + (calendar.get(MONTH) + 1) * 100 + calendar.get(DAY_OF_MONTH);
+        }
     }
 }
