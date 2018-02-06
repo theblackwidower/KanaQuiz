53f30b27c61e6b283266c10f86e213958d3f5d03
T Duke Perry <theblackwidower@noprestige.com>
2018-02-05 22:37:27 -0500

    Encapsulated map in WeightedList
    Edited WeightedList to contain a TreeMap object rather than extend the
    class. This allows me to fully control access to the list, rather than
    allow access through the TreeMap methods I haven't overridden, which
    could result in unknown side effects.

--- app/src/main/java/com/noprestige/kanaquiz/WeightedList.java
+++ app/src/main/java/com/noprestige/kanaquiz/WeightedList.java
@@ -1,17 +1,19 @@
 package com.noprestige.kanaquiz;
 
+import java.util.Collection;
 import java.util.Random;
 import java.util.TreeMap;
 
-class WeightedList<E> extends TreeMap<Integer, E>
+class WeightedList<E>
 {
+    private TreeMap<Integer, E> map = new TreeMap<>();
     private int maxValue = 0;
 
     private static Random rng = new Random();
 
     boolean add(int weight, E element)
     {
-        this.put(maxValue, element);
+        map.put(maxValue, element);
         maxValue += weight;
         return true;
     }
@@ -23,26 +25,25 @@ class WeightedList<E> extends TreeMap<Integer, E>
 
     E remove(int key)
     {
-        key = this.floorKey(key);
-        E returnValue = super.remove(key);
+        key = map.floorKey(key);
+        E returnValue = map.remove(key);
         if (returnValue == null)
             return null;
-        else if (this.higherKey(key) == null)
+        else if (map.higherKey(key) == null)
         {
             maxValue = key;
             return returnValue;
         }
         else
         {
-            Integer thisKey = this.higherKey(key);
+            Integer thisKey = map.higherKey(key);
             int gap = thisKey - key;
 
             while (thisKey != null)
             {
-                E currentElement = super.get(thisKey);
-                super.remove(thisKey);
-                super.put(thisKey - gap, currentElement);
-                thisKey = this.higherKey(thisKey);
+                E currentElement = map.remove(thisKey);
+                map.put(thisKey - gap, currentElement);
+                thisKey = map.higherKey(thisKey);
             }
 
             maxValue -= gap;
@@ -52,7 +53,7 @@ class WeightedList<E> extends TreeMap<Integer, E>
 
     int count()
     {
-        return super.size();
+        return map.size();
     }
 
     E get(int value)
@@ -60,7 +61,7 @@ class WeightedList<E> extends TreeMap<Integer, E>
         if (value > maxValue || value < 0)
             throw new NullPointerException();
         else
-            return this.get(this.floorKey(value));
+            return map.get(map.floorKey(value));
     }
 
     E getRandom()
@@ -75,10 +76,15 @@ class WeightedList<E> extends TreeMap<Integer, E>
 
     boolean merge(WeightedList<E> list)
     {
-        for (Integer oldKey : list.keySet())
-            this.put(this.maxValue + oldKey, list.get(oldKey));
+        for (Integer oldKey : list.map.keySet())
+            map.put(this.maxValue + oldKey, list.map.get(oldKey));
         this.maxValue += list.maxValue;
 
         return true;
+    }
+
+    Collection<E> values()
+    {
+        return map.values();
     }
 }
