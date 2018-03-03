5ab333b1df4ff13016ba7e665ffa0288040b0fb2
T Duke Perry <theblackwidower@noprestige.com>
2018-03-03 15:41:14 -0500

    Fixed a few oversights
    I forgot to double check the test fractions for simplicity in the last commit (bfafdd8d16ac76fe827390d9e0cbbfc77f2e9886). This resulted in a few false negatives. It has been fixed.

--- app/src/test/java/com/noprestige/kanaquiz/FractionTest.java
+++ app/src/test/java/com/noprestige/kanaquiz/FractionTest.java
@@ -77,7 +77,7 @@ public class FractionTest
         assertEquals(new Fraction(2f / 10f).toString(), "⅕");
         assertEquals(new Fraction(3f / 10f).toString(), "3⁄10");
         assertEquals(new Fraction(4f / 10f).toString(), "⅖");
-        assertEquals(new Fraction(5f / 10f).toString(), "5⁄10");
+        assertEquals(new Fraction(5f / 10f).toString(), "½");
         assertEquals(new Fraction(6f / 10f).toString(), "⅗");
         assertEquals(new Fraction(7f / 10f).toString(), "7⁄10");
         assertEquals(new Fraction(8f / 10f).toString(), "⅘");
@@ -87,7 +87,7 @@ public class FractionTest
         assertEquals(new Fraction(12f / 10f).toString(), "1⅕");
         assertEquals(new Fraction(13f / 10f).toString(), "1\u200B3⁄10");
         assertEquals(new Fraction(14f / 10f).toString(), "1⅖");
-        assertEquals(new Fraction(15f / 10f).toString(), "1\u200B5⁄10");
+        assertEquals(new Fraction(15f / 10f).toString(), "1½");
         assertEquals(new Fraction(16f / 10f).toString(), "1⅗");
         assertEquals(new Fraction(17f / 10f).toString(), "1\u200B7⁄10");
         assertEquals(new Fraction(18f / 10f).toString(), "1⅘");
@@ -96,25 +96,25 @@ public class FractionTest
 
         assertEquals(new Fraction(1f / 12f).toString(), "1⁄12");
         assertEquals(new Fraction(2f / 12f).toString(), "⅙");
-        assertEquals(new Fraction(3f / 12f).toString(), "3⁄12");
+        assertEquals(new Fraction(3f / 12f).toString(), "¼");
         assertEquals(new Fraction(4f / 12f).toString(), "⅓");
         assertEquals(new Fraction(5f / 12f).toString(), "5⁄12");
         assertEquals(new Fraction(6f / 12f).toString(), "½");
         assertEquals(new Fraction(7f / 12f).toString(), "7⁄12");
         assertEquals(new Fraction(8f / 12f).toString(), "⅔");
-        assertEquals(new Fraction(9f / 12f).toString(), "9⁄12");
+        assertEquals(new Fraction(9f / 12f).toString(), "¾");
         assertEquals(new Fraction(10f / 12f).toString(), "⅚");
         assertEquals(new Fraction(11f / 12f).toString(), "11⁄12");
         assertEquals(new Fraction(12f / 12f).toString(), "1");
         assertEquals(new Fraction(13f / 12f).toString(), "1\u200B1⁄12");
         assertEquals(new Fraction(14f / 12f).toString(), "1⅙");
-        assertEquals(new Fraction(15f / 12f).toString(), "1\u200B3⁄12");
+        assertEquals(new Fraction(15f / 12f).toString(), "1¼");
         assertEquals(new Fraction(16f / 12f).toString(), "1⅓");
         assertEquals(new Fraction(17f / 12f).toString(), "1\u200B5⁄12");
         assertEquals(new Fraction(18f / 12f).toString(), "1½");
         assertEquals(new Fraction(19f / 12f).toString(), "1\u200B7⁄12");
         assertEquals(new Fraction(20f / 12f).toString(), "1⅔");
-        assertEquals(new Fraction(21f / 12f).toString(), "1\u200B9⁄12");
+        assertEquals(new Fraction(21f / 12f).toString(), "1¾");
         assertEquals(new Fraction(22f / 12f).toString(), "1⅚");
         assertEquals(new Fraction(23f / 12f).toString(), "1\u200B11⁄12");
         assertEquals(new Fraction(24f / 12f).toString(), "2");
@@ -152,5 +152,4 @@ public class FractionTest
         assertEquals(new Fraction(31f / 16f).toString(), "1\u200B15⁄16");
         assertEquals(new Fraction(32f / 16f).toString(), "2");
     }
-
 }
