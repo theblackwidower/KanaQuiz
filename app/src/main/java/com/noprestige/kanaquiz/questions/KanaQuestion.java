98ced10b0f262d8680fd18e0ba0146f9ef96b63c
T Duke Perry <theblackwidower@noprestige.com>
2018-03-07 22:29:14 -0500

    Created isDigraph and isDiacritic methods
    These two methods could be used to identify which category a particular kana question might fall in. This will mainly be useful when I implement more advanced criteria for determining which answers to select for the multiple choice option.

--- app/src/main/java/com/noprestige/kanaquiz/questions/KanaQuestion.java
+++ app/src/main/java/com/noprestige/kanaquiz/questions/KanaQuestion.java
@@ -85,4 +85,39 @@ public class KanaQuestion
     {
         return kana + " = " + answer[0];
     }
+
+    public boolean isDigraph()
+    {
+        return kana.length() > 1;
+    }
+
+    public boolean isDiacritic()
+    {
+        int charCode = Character.codePointAt(kana, 0);
+
+        if (charCode < 0x3041 || charCode > 0x30FF)
+            return false; //not kana anyway
+        else if (charCode >= 0x30F7 && charCode <= 0x30FA)
+            return true; //special katakana diacritics
+
+        charCode %= 0x60; //can now run the calculations once for both hiragana and katakana
+
+        if ((charCode >= 0x40 && charCode <= 0x4B) || //early vowels
+                (charCode >= 0x0A && charCode <= 0x0E) || //N-set
+                (charCode >= 0x1E && charCode <= 0x33)) //M, Y, R, W-sets and N
+            return false;
+
+        else if (charCode >= 0x4C || charCode <= 0x03)
+            return (charCode % 2 == 0); //K and S-sets, and first few T-set chars
+
+        else if (charCode >= 0x04 && charCode <= 0x09)
+            return (charCode % 2 == 1); //last few T-set chars
+
+        else if (charCode >= 0x0F && charCode <= 0x1D)
+            return (charCode % 3 != 0); //H-set with two types of diacritics
+
+        else
+            return (charCode == 0x34 || charCode == 0x3E);
+        //two special characters at the end of the block, which I'm counting.
+    }
 }
