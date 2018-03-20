e63f563d07f3378660bbfb16b264a516a9ba10dd
T Duke Perry <theblackwidower@noprestige.com>
2018-03-20 12:40:04 -0400

    Cleaned up GojuonOrderTest
    Closed up some spacing, and segregated repeating code.

--- app/src/test/java/com/noprestige/kanaquiz/questions/GojuonOrderTest.java
+++ app/src/test/java/com/noprestige/kanaquiz/questions/GojuonOrderTest.java
@@ -12,183 +12,137 @@ public class GojuonOrderTest
 {
     private static final String[] EXPECTED_ORDER_DEFAULT = {
             "a", "i", "u", "e", "o",
-
             "ka", "ki", "ku", "ke", "ko",
             "kya", "kyu", "kyo",
             "ga", "gi", "gu", "ge", "go",
             "gya", "gyu", "gyo",
-
             "sa", "shi", "su", "se", "so",
             "sha", "shu", "sho",
             "za", "ji", "zu", "ze", "zo",
             "ja", "ju", "jo",
-
             "ta", "chi", "tsu", "te", "to",
             "cha", "chu", "cho",
             "da", /*"ji", "zu",*/ "de", "do",
             //"ja", "ju", "jo",
-
             "na", "ni", "nu", "ne", "no",
             "nya", "nyu", "nyo",
-
             "ha", "hi", "fu", "he", "ho",
             "hya", "hyu", "hyo",
             "ba", "bi", "bu", "be", "bo",
             "bya", "byu", "byo",
             "pa", "pi", "pu", "pe", "po",
             "pya", "pyu", "pyo",
-
             "ma", "mi", "mu", "me", "mo",
             "mya", "myu", "myo",
-
             "ya", "yu", "yo",
-
             "ra", "ri", "ru", "re", "ro",
             "rya", "ryu", "ryo",
-
             "wa", "wi", "we", "wo",
             "n"};
 
-
     private static final String[] EXPECTED_ORDER_HEPBURN = {
             "a", "i", "u", "e", "o",
-
             "ka", "ki", "ku", "ke", "ko",
             "kya", "kyu", "kyo",
             "ga", "gi", "gu", "ge", "go",
             "gya", "gyu", "gyo",
-
             "sa", "shi", "su", "se", "so",
             "sha", "shu", "sho",
             "za", "ji", "zu", "ze", "zo",
             "ja", "ju", "jo",
-
             "ta", "chi", "tsu", "te", "to",
             "cha", "chu", "cho",
             "da", /*"ji", "zu",*/ "de", "do",
             //"ja", "ju", "jo",
-
             "na", "ni", "nu", "ne", "no",
             "nya", "nyu", "nyo",
-
             "ha", "hi", "fu", "he", "ho",
             "hya", "hyu", "hyo",
             "ba", "bi", "bu", "be", "bo",
             "bya", "byu", "byo",
             "pa", "pi", "pu", "pe", "po",
             "pya", "pyu", "pyo",
-
             "ma", "mi", "mu", "me", "mo",
             "mya", "myu", "myo",
-
             "ya", "yu", "yo",
-
             "ra", "ri", "ru", "re", "ro",
             "rya", "ryu", "ryo",
-
             "wa", //"i", "e", "o",
             "n"};
 
-
     private static final String[] EXPECTED_ORDER_NIHON = {
             "a", "i", "u", "e", "o",
-
             "ka", "ki", "ku", "ke", "ko",
             "kya", "kyu", "kyo",
             "ga", "gi", "gu", "ge", "go",
             "gya", "gyu", "gyo",
-
             "sa", "si", "su", "se", "so",
             "sya", "syu", "syo",
             "za", "zi", "zu", "ze", "zo",
             "zya", "zyu", "zyo",
-
             "ta", "ti", "tu", "te", "to",
             "tya", "tyu", "tyo",
             "da", "di", "du", "de", "do",
             "dya", "dyu", "dyo",
-
             "na", "ni", "nu", "ne", "no",
             "nya", "nyu", "nyo",
-
             "ha", "hi", "hu", "he", "ho",
             "hya", "hyu", "hyo",
             "ba", "bi", "bu", "be", "bo",
             "bya", "byu", "byo",
             "pa", "pi", "pu", "pe", "po",
             "pya", "pyu", "pyo",
-
             "ma", "mi", "mu", "me", "mo",
             "mya", "myu", "myo",
-
             "ya", "yu", "yo",
-
             "ra", "ri", "ru", "re", "ro",
             "rya", "ryu", "ryo",
-
             "wa", "wi", "we", "wo",
             "n"};
 
-
     private static final String[] EXPECTED_ORDER_KUNREI = {
             "a", "i", "u", "e", "o",
-
             "ka", "ki", "ku", "ke", "ko",
             "kya", "kyu", "kyo",
             "ga", "gi", "gu", "ge", "go",
             "gya", "gyu", "gyo",
-
             "sa", "si", "su", "se", "so",
             "sya", "syu", "syo",
             "za", "zi", "zu", "ze", "zo",
             "zya", "zyu", "zyo",
-
             "ta", "ti", "tu", "te", "to",
             "tya", "tyu", "tyo",
             "da", /*"zi", "zu",*/ "de", "do",
             //"zya", "zyu", "zyo",
-
             "na", "ni", "nu", "ne", "no",
             "nya", "nyu", "nyo",
-
             "ha", "hi", "hu", "he", "ho",
             "hya", "hyu", "hyo",
             "ba", "bi", "bu", "be", "bo",
             "bya", "byu", "byo",
             "pa", "pi", "pu", "pe", "po",
             "pya", "pyu", "pyo",
-
             "ma", "mi", "mu", "me", "mo",
             "mya", "myu", "myo",
-
             "ya", "yu", "yo",
-
             "ra", "ri", "ru", "re", "ro",
             "rya", "ryu", "ryo",
-
             "wa", //"i", "e", "o",
             "n"};
-
 
     @Test
     public void sortTest() throws Exception
     {
+        checkOrder(EXPECTED_ORDER_DEFAULT);
+        checkOrder(EXPECTED_ORDER_HEPBURN);
+        checkOrder(EXPECTED_ORDER_NIHON);
+        checkOrder(EXPECTED_ORDER_KUNREI);
+    }
+
+    private void checkOrder(String[] testArray)
+    {
         Set<String> sortedSet = new TreeSet<>(new GojuonOrder());
-
-        sortedSet.addAll(Arrays.asList(EXPECTED_ORDER_DEFAULT));
-        assertArrayEquals(sortedSet.toArray(), EXPECTED_ORDER_DEFAULT);
-        sortedSet.clear();
-
-        sortedSet.addAll(Arrays.asList(EXPECTED_ORDER_HEPBURN));
-        assertArrayEquals(sortedSet.toArray(), EXPECTED_ORDER_HEPBURN);
-        sortedSet.clear();
-
-        sortedSet.addAll(Arrays.asList(EXPECTED_ORDER_NIHON));
-        assertArrayEquals(sortedSet.toArray(), EXPECTED_ORDER_NIHON);
-        sortedSet.clear();
-
-        sortedSet.addAll(Arrays.asList(EXPECTED_ORDER_KUNREI));
-        assertArrayEquals(sortedSet.toArray(), EXPECTED_ORDER_KUNREI);
-        sortedSet.clear();
+        sortedSet.addAll(Arrays.asList(testArray));
+        assertArrayEquals(sortedSet.toArray(), testArray);
     }
 }
