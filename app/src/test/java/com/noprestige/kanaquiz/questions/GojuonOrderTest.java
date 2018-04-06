5950fb2f3c3a511f533d7002458e704d177dae9c
T Duke Perry <theblackwidower@noprestige.com>
2018-04-05 21:46:16 -0400

    Added tests to GojuonOrder
    Additional tests have been added to test if it works with capital letters, and for multiple sets of characters.

--- app/src/test/java/com/noprestige/kanaquiz/questions/GojuonOrderTest.java
+++ app/src/test/java/com/noprestige/kanaquiz/questions/GojuonOrderTest.java
@@ -50,6 +50,21 @@ public class GojuonOrderTest
             "rya", "ryu", "ryo", "wa", /*"i", "e", "o",*/ "n", "l", "q", "v", "x"
     };
 
+    private static final String[] EXPECTED_ORDER_NIHON_CAPITAL = {
+            "A", "I", "U", "E", "O", "KA", "KI", "KU", "KE", "KO", "KYA", "KYU", "KYO", "GA", "GI", "GU", "GE", "GO",
+            "GYA", "GYU", "GYO", "SA", "SI", "SU", "SE", "SO", "SYA", "SYU", "SYO", "ZA", "ZI", "ZU", "ZE", "ZO", "ZYA",
+            "ZYU", "ZYO", "TA", "TI", "TU", "TE", "TO", "TYA", "TYU", "TYO", "DA", "DI", "DU", "DE", "DO", "DYA", "DYU",
+            "DYO", "NA", "NI", "NU", "NE", "NO", "NYA", "NYU", "NYO", "HA", "HI", "HU", "HE", "HO", "HYA", "HYU", "HYO",
+            "BA", "BI", "BU", "BE", "BO", "BYA", "BYU", "BYO", "PA", "PI", "PU", "PE", "PO", "PYA", "PYU", "PYO", "MA",
+            "MI", "MU", "ME", "MO", "MYA", "MYU", "MYO", "YA", "YU", "YO", "RA", "RI", "RU", "RE", "RO", "RYA", "RYU",
+            "RYO", "WA", "WI", "WE", "WO", "N", "L"
+    };
+
+    private static final String[] EXPECTED_ORDER_MULTICHAR = {
+            "iau", "iae", "ie", "ieo", "ue", "ueo", "kaa", "ki", "kiu", "ku", "kua", "kuu", "kuka", "kuko", "kyu",
+            "kyui", "kyuka", "kyuko", "kyo", "n"
+    };
+
     @Test
     public void sortTest() throws Exception
     {
@@ -57,6 +72,9 @@ public class GojuonOrderTest
         checkOrder(EXPECTED_ORDER_HEPBURN);
         checkOrder(EXPECTED_ORDER_NIHON);
         checkOrder(EXPECTED_ORDER_KUNREI);
+
+        checkOrder(EXPECTED_ORDER_NIHON_CAPITAL);
+        checkOrder(EXPECTED_ORDER_MULTICHAR);
     }
 
     private void checkOrder(String[] testArray)
