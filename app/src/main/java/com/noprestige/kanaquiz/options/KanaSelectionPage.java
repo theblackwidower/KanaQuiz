de136237ff16f369bfad0f5546a55e521f1e130d
T Duke Perry <theblackwidower@noprestige.com>
2018-09-09 18:10:20 -0400

    Fixed reloading issue on Kana Selection screen
    Fixed the issue causing all items to be deselected on the Kana Selection screen whenever the fragment is reloaded. This allows me to enable rotation on the Kana Selection screen, and potentially add a third section to it, without the accidental clearing of selections the user already made.

--- app/src/main/java/com/noprestige/kanaquiz/options/KanaSelectionPage.java
+++ app/src/main/java/com/noprestige/kanaquiz/options/KanaSelectionPage.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Checkable;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
@@ -14,6 +15,8 @@ import static com.noprestige.kanaquiz.questions.QuestionManagement.KATAKANA;
 public class KanaSelectionPage extends Fragment
 {
     private static final String ARG_PAGE_NUMBER = "position";
+    private static final String ARG_ITEM_STATES = "states";
+    LinearLayout screen;
 
     public static KanaSelectionPage newInstance(int id)
     {
@@ -27,10 +30,6 @@ public class KanaSelectionPage extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        //TODO: Fix reloading issue.
-        //      All KanaSelectionItems end up deselected when the Fragment is reloaded,
-        //      either through screen rotation, or if there is a third tab.
-        LinearLayout screen;
         switch (getArguments().getInt(ARG_PAGE_NUMBER, -1))
         {
             case 0:
@@ -45,5 +44,34 @@ public class KanaSelectionPage extends Fragment
         ScrollView scrollView = new ScrollView(getContext());
         scrollView.addView(screen);
         return scrollView;
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        //This block of code could also work in the onResume method
+        boolean[] record = getArguments().getBooleanArray(ARG_ITEM_STATES);
+        if (record != null)
+        {
+            int count = Math.min(screen.getChildCount(), record.length);
+
+            for (int i = 0; i < count; i++)
+                ((Checkable) screen.getChildAt(i)).setChecked(record[i]);
+        }
+    }
+
+    @Override
+    public void onDestroyView()
+    {
+        super.onDestroyView();
+        //This block of code could also work in the onPause and onStop methods
+        int count = screen.getChildCount();
+        boolean[] record = new boolean[count];
+
+        for (int i = 0; i < count; i++)
+            record[i] = ((Checkable) screen.getChildAt(i)).isChecked();
+
+        getArguments().putBooleanArray(ARG_ITEM_STATES, record);
     }
 }
