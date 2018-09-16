561212a7cadef3a0f5edb6003332d748d28631df
T Duke Perry <theblackwidower@noprestige.com>
2018-09-15 21:22:52 -0400

    Expanded TypeConversionTest
    Now tests the fromTimestamp method, and that null values are passed through appropriately with no change.

--- app/src/test/java/com/noprestige/kanaquiz/logs/TypeConversionTest.java
+++ app/src/test/java/com/noprestige/kanaquiz/logs/TypeConversionTest.java
@@ -5,6 +5,7 @@ import org.joda.time.LocalDate;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class TypeConversionTest
@@ -13,6 +14,11 @@ public class TypeConversionTest
     {
         LocalDate dateObject = new LocalDate(year, month, day);
         assertThat(LogTypeConversion.dateToTimestamp(dateObject), is(all));
+
+        LocalDate testObject = LogTypeConversion.fromTimestamp(all);
+        assertThat(testObject.getYear(), is(year));
+        assertThat(testObject.getMonthOfYear(), is(month));
+        assertThat(testObject.getDayOfMonth(), is(day));
     }
 
     @Test
@@ -22,5 +28,8 @@ public class TypeConversionTest
         assertConversion(2015, DateTimeConstants.JUNE, 12, 20150612);
         assertConversion(1995, DateTimeConstants.JANUARY, 5, 19950105);
         assertConversion(2025, DateTimeConstants.SEPTEMBER, 21, 20250921);
+
+        assertThat(LogTypeConversion.dateToTimestamp(null), is(nullValue()));
+        assertThat(LogTypeConversion.fromTimestamp(null), is(nullValue()));
     }
 }
