d0333d750ae174bc97368fbd52be737ae95bb05d
T Duke Perry <theblackwidower@noprestige.com>
2018-09-18 06:54:14 -0400

    Cleaned up repeating code
    There was a lot of repeating code in XmlParserTest for examining the KanaQuestion objects, so I split that off into its own method, which is passed merely the salient points that need to be tested.

--- app/src/androidTest/java/com/noprestige/kanaquiz/questions/XmlParserTest.java
+++ app/src/androidTest/java/com/noprestige/kanaquiz/questions/XmlParserTest.java
@@ -41,102 +41,70 @@ public class XmlParserTest
         assertThat(setTitleList.get(0), is("First Set"));
         assertThat(setNoDiacriticsTitleList.get(0), is(nullValue()));
 
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][0].getKana(), is("あ"));
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][0].fetchRomanji(null), is("a"));
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][1].getKana(), is("い"));
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][1].fetchRomanji(null), is("i"));
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][2].getKana(), is("う"));
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][2].fetchRomanji(null), is("u"));
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][3].getKana(), is("え"));
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][3].fetchRomanji(null), is("e"));
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][4].getKana(), is("お"));
-        assertThat(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][4].fetchRomanji(null), is("o"));
+        assertKanaQuestion(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][0], "あ", "a");
+        assertKanaQuestion(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][1], "い", "i");
+        assertKanaQuestion(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][2], "う", "u");
+        assertKanaQuestion(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][3], "え", "e");
+        assertKanaQuestion(kanaSetList.get(0)[NO_DIACRITIC.ordinal()][0][4], "お", "o");
 
 
         assertThat(prefIdList.get(1), is("set2"));
         assertThat(setTitleList.get(1), is("Second Set"));
         assertThat(setNoDiacriticsTitleList.get(1), is(nullValue()));
 
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][0].getKana(), is("な"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][0].fetchRomanji(null), is("na"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][1].getKana(), is("に"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][1].fetchRomanji(null), is("ni"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][2].getKana(), is("ぬ"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][2].fetchRomanji(null), is("nu"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][3].getKana(), is("ね"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][3].fetchRomanji(null), is("ne"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][4].getKana(), is("の"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][4].fetchRomanji(null), is("no"));
+        assertKanaQuestion(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][0], "な", "na");
+        assertKanaQuestion(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][1], "に", "ni");
+        assertKanaQuestion(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][2], "ぬ", "nu");
+        assertKanaQuestion(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][3], "ね", "ne");
+        assertKanaQuestion(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][0][4], "の", "no");
 
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][1][0].getKana(), is("にゃ"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][1][0].fetchRomanji(null), is("nya"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][1][1].getKana(), is("にゅ"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][1][1].fetchRomanji(null), is("nyu"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][1][2].getKana(), is("にょ"));
-        assertThat(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][1][2].fetchRomanji(null), is("nyo"));
+        assertKanaQuestion(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][1][0], "にゃ", "nya");
+        assertKanaQuestion(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][1][1], "にゅ", "nyu");
+        assertKanaQuestion(kanaSetList.get(1)[NO_DIACRITIC.ordinal()][1][2], "にょ", "nyo");
 
 
         assertThat(prefIdList.get(2), is("set3"));
         assertThat(setTitleList.get(2), is("Third Set with Diacritics"));
         assertThat(setNoDiacriticsTitleList.get(2), is("Third Set"));
 
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][0].getKana(), is("は"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][0].fetchRomanji(null), is("ha"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][1].getKana(), is("ひ"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][1].fetchRomanji(null), is("hi"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][2].getKana(), is("ふ"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][2].fetchRomanji(null), is("fu"));
+        assertKanaQuestion(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][0], "は", "ha");
+        assertKanaQuestion(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][1], "ひ", "hi");
+        assertKanaQuestion(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][2], "ふ", "fu");
         assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][2].fetchRomanji(NIHON), is("hu"));
         assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][2].fetchRomanji(KUNREI), is("hu"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][3].getKana(), is("へ"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][3].fetchRomanji(null), is("he"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][4].getKana(), is("ほ"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][4].fetchRomanji(null), is("ho"));
+        assertKanaQuestion(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][3], "へ", "he");
+        assertKanaQuestion(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][0][4], "ほ", "ho");
 
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][0].getKana(), is("ば"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][0].fetchRomanji(null), is("ba"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][1].getKana(), is("び"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][1].fetchRomanji(null), is("bi"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][2].getKana(), is("ぶ"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][2].fetchRomanji(null), is("bu"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][3].getKana(), is("べ"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][3].fetchRomanji(null), is("be"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][4].getKana(), is("ぼ"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][0][4].fetchRomanji(null), is("bo"));
+        assertKanaQuestion(kanaSetList.get(2)[DAKUTEN.ordinal()][0][0], "ば", "ba");
+        assertKanaQuestion(kanaSetList.get(2)[DAKUTEN.ordinal()][0][1], "び", "bi");
+        assertKanaQuestion(kanaSetList.get(2)[DAKUTEN.ordinal()][0][2], "ぶ", "bu");
+        assertKanaQuestion(kanaSetList.get(2)[DAKUTEN.ordinal()][0][3], "べ", "be");
+        assertKanaQuestion(kanaSetList.get(2)[DAKUTEN.ordinal()][0][4], "ぼ", "bo");
 
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][0].getKana(), is("ぱ"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][0].fetchRomanji(null), is("pa"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][1].getKana(), is("ぴ"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][1].fetchRomanji(null), is("pi"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][2].getKana(), is("ぷ"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][2].fetchRomanji(null), is("pu"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][3].getKana(), is("ぺ"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][3].fetchRomanji(null), is("pe"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][4].getKana(), is("ぽ"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][4].fetchRomanji(null), is("po"));
+        assertKanaQuestion(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][0], "ぱ", "pa");
+        assertKanaQuestion(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][1], "ぴ", "pi");
+        assertKanaQuestion(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][2], "ぷ", "pu");
+        assertKanaQuestion(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][3], "ぺ", "pe");
+        assertKanaQuestion(kanaSetList.get(2)[HANDAKUTEN.ordinal()][0][4], "ぽ", "po");
 
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][1][0].getKana(), is("ひゃ"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][1][0].fetchRomanji(null), is("hya"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][1][1].getKana(), is("ひゅ"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][1][1].fetchRomanji(null), is("hyu"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][1][2].getKana(), is("ひょ"));
-        assertThat(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][1][2].fetchRomanji(null), is("hyo"));
+        assertKanaQuestion(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][1][0], "ひゃ", "hya");
+        assertKanaQuestion(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][1][1], "ひゅ", "hyu");
+        assertKanaQuestion(kanaSetList.get(2)[NO_DIACRITIC.ordinal()][1][2], "ひょ", "hyo");
 
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][1][0].getKana(), is("びゃ"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][1][0].fetchRomanji(null), is("bya"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][1][1].getKana(), is("びゅ"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][1][1].fetchRomanji(null), is("byu"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][1][2].getKana(), is("びょ"));
-        assertThat(kanaSetList.get(2)[DAKUTEN.ordinal()][1][2].fetchRomanji(null), is("byo"));
+        assertKanaQuestion(kanaSetList.get(2)[DAKUTEN.ordinal()][1][0], "びゃ", "bya");
+        assertKanaQuestion(kanaSetList.get(2)[DAKUTEN.ordinal()][1][1], "びゅ", "byu");
+        assertKanaQuestion(kanaSetList.get(2)[DAKUTEN.ordinal()][1][2], "びょ", "byo");
 
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][1][0].getKana(), is("ぴゃ"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][1][0].fetchRomanji(null), is("pya"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][1][1].getKana(), is("ぴゅ"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][1][1].fetchRomanji(null), is("pyu"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][1][2].getKana(), is("ぴょ"));
-        assertThat(kanaSetList.get(2)[HANDAKUTEN.ordinal()][1][2].fetchRomanji(null), is("pyo"));
+        assertKanaQuestion(kanaSetList.get(2)[HANDAKUTEN.ordinal()][1][0], "ぴゃ", "pya");
+        assertKanaQuestion(kanaSetList.get(2)[HANDAKUTEN.ordinal()][1][1], "ぴゅ", "pyu");
+        assertKanaQuestion(kanaSetList.get(2)[HANDAKUTEN.ordinal()][1][2], "ぴょ", "pyo");
 
-        assertThat(kanaSetList.get(2)[CONSONANT.ordinal()][0][0].getKana(), is("ん"));
-        assertThat(kanaSetList.get(2)[CONSONANT.ordinal()][0][0].fetchRomanji(null), is("n"));
+        assertKanaQuestion(kanaSetList.get(2)[CONSONANT.ordinal()][0][0], "ん", "n");
+    }
+
+    private void assertKanaQuestion(KanaQuestion question, String expectedKana, String expectedRomanji)
+    {
+        assertThat(question.getKana(), is(expectedKana));
+        assertThat(question.fetchRomanji(null), is(expectedRomanji));
     }
 }
