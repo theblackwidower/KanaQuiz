4fce65ff7ed1237b44f27ba7ba3df45e6b660f26
T Duke Perry <theblackwidower@noprestige.com>
2018-10-16 01:33:26 -0400

    Added no reference hook
    Added a hook to the reference screen for it to display a special message if no reference items are to be displayed. This should only happen if no items are selected on the KanaSelection screen, and the full reference option is disabled, which would be an odd scenario. But without this hook, we just have a blank screen in this scenario, which tells the user nothing, and could mean the page is still loading. So I thought, we should display a message here.

--- app/src/main/java/com/noprestige/kanaquiz/reference/ReferenceScreen.java
+++ app/src/main/java/com/noprestige/kanaquiz/reference/ReferenceScreen.java
@@ -1,12 +1,17 @@
 package com.noprestige.kanaquiz.reference;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.noprestige.kanaquiz.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
+import static android.util.TypedValue.COMPLEX_UNIT_SP;
 
 public class ReferenceScreen extends AppCompatActivity
 {
@@ -18,6 +23,19 @@ public class ReferenceScreen extends AppCompatActivity
 
         ViewPager viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(new ReferencePager(getSupportFragmentManager(), this));
-        ((TabLayout) findViewById(R.id.tabLayout)).setupWithViewPager(viewPager);
+        if (viewPager.getChildCount() == 0)
+        {
+            TextView lblEmpty = new TextView(this);
+            lblEmpty.setText(R.string.no_reference);
+            lblEmpty.setTextSize(COMPLEX_UNIT_SP, 24);
+            lblEmpty.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            lblEmpty.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            ViewGroup layout = (ViewGroup) viewPager.getParent();
+            layout.removeAllViews();
+            layout.addView(lblEmpty);
+        }
+        else
+            ((TabLayout) findViewById(R.id.tabLayout)).setupWithViewPager(viewPager);
     }
 }
