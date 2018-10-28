7ea094d058eb937f37bdbef9a73e27e0c76644f2
T Duke Perry <theblackwidower@noprestige.com>
2018-10-27 21:36:08 -0400

    More reorganizing of migration code
    Moved some code around in the LogDatabase class, so all the code that migrates the kana_records table, and all the code that migrates the incorrect_answers table, are together in single blocks and not mingling with each other. Also, cleaned up some line spacing.

--- app/src/main/java/com/noprestige/kanaquiz/logs/LogDatabase.java
+++ app/src/main/java/com/noprestige/kanaquiz/logs/LogDatabase.java
@@ -35,11 +35,9 @@ public abstract class LogDatabase extends RoomDatabase
             database.execSQL("ALTER TABLE daily_record RENAME TO old_daily_record");
             database.execSQL("CREATE TABLE IF NOT EXISTS daily_record (date INTEGER, correct_answers REAL NOT NULL, " +
                     "total_answers INTEGER NOT NULL, PRIMARY KEY(date))");
-
             database.execSQL(
                     "INSERT INTO daily_record (date, correct_answers, total_answers) SELECT date, correct_answers, " +
                             "correct_answers + incorrect_answers FROM old_daily_record");
-
             database.execSQL("DROP TABLE old_daily_record");
         }
     };
@@ -53,21 +51,19 @@ public abstract class LogDatabase extends RoomDatabase
 
             //rebuilding tables because ALTER TABLE can't alter, or even add to, the primary key
             database.execSQL("ALTER TABLE kana_records RENAME TO old_kana_records");
-            database.execSQL("ALTER TABLE incorrect_answers RENAME TO old_incorrect_answers");
-
             database.execSQL("CREATE TABLE IF NOT EXISTS kana_records (date INTEGER NOT NULL, kana TEXT NOT NULL, " +
                     "correct_answers INTEGER NOT NULL, incorrect_answers INTEGER NOT NULL, PRIMARY KEY(date, " +
                     "kana))");
-            database.execSQL(
-                    "CREATE TABLE IF NOT EXISTS incorrect_answers (date INTEGER NOT NULL, kana TEXT NOT NULL, " +
-                            "incorrect_romanji TEXT NOT NULL, occurrences INTEGER NOT NULL, PRIMARY KEY(date, kana, " +
-                            "incorrect_romanji))");
-
             database.execSQL(
                     "INSERT INTO kana_records (date, kana, correct_answers, incorrect_answers) SELECT '" + currentDate +
                             "', kana, correct_answers, incorrect_answers FROM old_kana_records");
             database.execSQL("DROP TABLE old_kana_records");
 
+            database.execSQL("ALTER TABLE incorrect_answers RENAME TO old_incorrect_answers");
+            database.execSQL(
+                    "CREATE TABLE IF NOT EXISTS incorrect_answers (date INTEGER NOT NULL, kana TEXT NOT NULL, " +
+                            "incorrect_romanji TEXT NOT NULL, occurrences INTEGER NOT NULL, PRIMARY KEY(date, kana, " +
+                            "incorrect_romanji))");
             database.execSQL("INSERT INTO incorrect_answers (date, kana, incorrect_romanji, occurrences) SELECT '" +
                     currentDate + "', kana, incorrect_romanji, occurrences FROM old_incorrect_answers");
             database.execSQL("DROP TABLE old_incorrect_answers");
