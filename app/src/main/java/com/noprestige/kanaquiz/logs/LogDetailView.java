66ea523be06bef70517b7c4532ad30c8cd2e06ba
T Duke Perry <theblackwidower@noprestige.com>
2018-11-08 02:03:19 -0500

    Switched out graph library
    Sick and tired of all the problems I was experiencing with the bar graphs from the GraphView library, I decided to change it out with the MPAndroidChart library. It handles bar charts in a much more intuitive way, and requires less faffing about, specifically with regards to labels on the X-axis. I think this is mainly because the GraphView library tries to handle all types of graphs in the same way, using the same GraphView class for all implementations, but the MPAndroidChart library handles things differently, using specific view classes for specific types of charts. So there's no attempt to combine the functionality of line graphs and bar graphs in the same view. There's also no weird graphical glitches. Overall, it's just a better library.

--- app/src/main/java/com/noprestige/kanaquiz/logs/LogDetailView.java
+++ app/src/main/java/com/noprestige/kanaquiz/logs/LogDetailView.java
@@ -8,11 +8,11 @@ import android.util.TypedValue;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.jjoe64.graphview.DefaultLabelFormatter;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.PointsGraphSeries;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.noprestige.kanaquiz.R;
 
 import org.threeten.bp.LocalDate;
@@ -33,14 +33,14 @@ public class LogDetailView extends AppCompatActivity
         @SuppressLint("StaticFieldLeak")
         TextView lblLogMessage;
         @SuppressLint("StaticFieldLeak")
-        GraphView logGraph;
-        BarGraphSeries<DataPoint> graphSeries;
+        BarChart logGraph;
+        List<BarEntry> graphSeries;
         List<String> staticLabels;
 
         @Override
         protected void onPreExecute()
         {
-            graphSeries = new BarGraphSeries<>();
+            graphSeries = new ArrayList<>();
             staticLabels = new ArrayList<>();
         }
 
@@ -53,40 +53,31 @@ public class LogDetailView extends AppCompatActivity
 
             QuestionRecord[] records = LogDatabase.DAO.getDatesQuestionRecords(activity[0].date);
 
-            logGraph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter()
+            logGraph.getXAxis().setValueFormatter((value, axis) ->
             {
-                @Override
-                public String formatLabel(double value, boolean isValueX)
-                {
-                    if (isValueX)
-                    {
-                        int key = (int) Math.floor(value);
-                        if ((key >= 0) && (key < staticLabels.size()))
-                        {
-                            double diff = value - key;
-                            if ((diff >= 0.499) && (diff <= 0.501))
-                                return staticLabels.get(key);
-                        }
-                        return null;
-                    }
-                    else
-                        return super.formatLabel(value, isValueX);
-                }
+                int key = Math.round(value);
+                if ((key >= 0) && (key < staticLabels.size()))
+                    return staticLabels.get(key);
+                return "";
             });
 
             if (records.length == 0)
                 return 0;
 
-            graphSeries.setSpacing(10);
-            graphSeries.setDataWidth(1.0);
+            logGraph.getAxisLeft().setAxisMaximum(100);
+            logGraph.getAxisLeft().setAxisMinimum(0);
+            logGraph.getAxisRight().setEnabled(false);
+            logGraph.getXAxis().setGranularity(1);
+            logGraph.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+            logGraph.getLegend().setEnabled(false);
 
             for (QuestionRecord record : records)
             {
                 LogDetailItem output = new LogDetailItem(activity[0].getBaseContext());
                 output.setFromRecord(record);
 
-                graphSeries.appendData(new DataPoint(staticLabels.size() + 0.5, (record.getCorrectAnswers() * 100) /
-                        (record.getCorrectAnswers() + record.getIncorrectAnswers())), true, 1000, true);
+                graphSeries.add(new BarEntry(staticLabels.size(), (record.getCorrectAnswers() * 100) /
+                        (record.getCorrectAnswers() + record.getIncorrectAnswers())));
 
                 staticLabels.add(record.getQuestion());
 
@@ -95,6 +86,15 @@ public class LogDetailView extends AppCompatActivity
                 else
                     publishProgress(output);
             }
+
+            BarData data = new BarData(new BarDataSet(graphSeries, null));
+            logGraph.setData(data);
+
+            data.setBarWidth(0.9f);
+            data.setDrawValues(false);
+
+            logGraph.invalidate();
+
             return records.length;
         }
 
@@ -102,10 +102,6 @@ public class LogDetailView extends AppCompatActivity
         protected void onProgressUpdate(LogDetailItem... item)
         {
             layout.addView(item[0], layout.getChildCount() - 1);
-            logGraph.getViewport().setMaxX(staticLabels.size());
-            logGraph.removeAllSeries();
-            logGraph.addSeries(graphSeries);
-            logGraph.getGridLabelRenderer().setNumHorizontalLabels(staticLabels.size() * 2);
         }
 
         @Override
@@ -120,15 +116,7 @@ public class LogDetailView extends AppCompatActivity
         protected void onPostExecute(Integer count)
         {
             if (count > 0)
-            {
                 layout.removeView(lblLogMessage);
-
-                PointsGraphSeries<DataPoint> graphPadding = new PointsGraphSeries<>();
-                graphPadding.setSize(0);
-                graphPadding.appendData(new DataPoint(0, 0), true, 1000, true);
-                graphPadding.appendData(new DataPoint(staticLabels.size(), 0), true, 1000, true);
-                logGraph.addSeries(graphPadding);
-            }
             else
                 lblLogMessage.setText(R.string.no_logs);
         }
@@ -141,7 +129,7 @@ public class LogDetailView extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_view);
+        setContentView(R.layout.activity_log_detail_view);
         onConfigurationChanged(getResources().getConfiguration());
 
         //ref: https://stackoverflow.com/a/3913735/3582371
@@ -152,18 +140,6 @@ public class LogDetailView extends AppCompatActivity
 
         fetchThread = new FetchLogDetails();
         fetchThread.execute(this);
-
-        GraphView logGraph = findViewById(R.id.logGraph);
-
-        logGraph.getViewport().setYAxisBoundsManual(true);
-        logGraph.getViewport().setMinY(0);
-        logGraph.getViewport().setMaxY(100);
-        logGraph.getViewport().setScalable(true); // X-axis zooming and scrolling
-        logGraph.getViewport().setXAxisBoundsManual(true);
-        logGraph.getViewport().setMinX(0);
-        logGraph.getGridLabelRenderer().setLabelVerticalWidth(Math.round(TypedValue
-                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20,
-                        getApplicationContext().getResources().getDisplayMetrics())));
     }
 
     @Override
@@ -201,7 +177,7 @@ public class LogDetailView extends AppCompatActivity
             listWidth = MATCH_PARENT;
             listHeight = 0;
         }
-        ((LinearLayout) findViewById(R.id.activity_log_view)).setOrientation(viewOrientation);
+        ((LinearLayout) findViewById(R.id.activity_log_detail_view)).setOrientation(viewOrientation);
         findViewById(R.id.logGraph).setLayoutParams(new LinearLayout.LayoutParams(graphWidth, graphHeight));
         findViewById(R.id.logViewScroll).setLayoutParams(new LinearLayout.LayoutParams(listWidth, listHeight, 1));
     }
