322061ae6c78f7a458ecf582065ee3a337553174
T Duke Perry <theblackwidower@noprestige.com>
2018-12-09 06:22:16 -0500

    Added alerts for Font Provider errors
    The new Font Provider system has a major flaw: There's an external app the user needs to install, otherwise the fonts won't work properly. And if they don't know to install the app, then the app's fonts will continually be out of whack. So I added a quick alert that checks if the Font Provider app is not installed, and in that case tells the user they should install the app if they want decent fonts. There's also a second alert telling the user if the app is out of date, and that they should update it. I wanted to add two links, one to Google Play, the other to GitHub. But seems there's a limit to how many buttons there can be on an AlertDialog. I think I have a workaround, but I'll have to experiment a bit.

--- app/src/main/java/com/noprestige/kanaquiz/themes/ThemeManager.java
+++ app/src/main/java/com/noprestige/kanaquiz/themes/ThemeManager.java
@@ -1,13 +1,18 @@
 package com.noprestige.kanaquiz.themes;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 
 import com.noprestige.kanaquiz.R;
 import com.noprestige.kanaquiz.options.OptionsControl;
+
+import org.threeten.bp.LocalDate;
 
 import moe.shizuku.fontprovider.FontProviderClient;
 import moe.shizuku.fontprovider.FontRequest;
@@ -54,7 +59,51 @@ public final class ThemeManager
 
     public static void setTheme(Activity activity)
     {
-        if (!isFontInitialized)
+        int code = FontProviderClient.checkAvailability(activity);
+        if (code == FontProviderClient.FontProviderAvailability.NOT_INSTALLED)
+        {
+            if (!OptionsControl.getBoolean(R.string.prefid_ignore_font_provider))
+            {
+                LocalDate remindDate = OptionsControl.getDate(R.string.prefid_font_remind_date);
+                if ((remindDate == null) || remindDate.isBefore(LocalDate.now()))
+                {
+                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+                    dialogBuilder.setMessage(R.string.font_provider_install_request);
+                    dialogBuilder.setPositiveButton(R.string.get_on_google_play, (dialog, which) ->
+                    {
+                        activity.startActivity(new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("https://play.google.com/store/apps/details?id=moe.shizuku.fontprovider")));
+//                        activity.startActivity(new Intent(Intent.ACTION_VIEW,
+//                                Uri.parse("https://github.com/RikkaApps/FontProvider/releases/")));
+                    });
+                    dialogBuilder.setNegativeButton(R.string.remind_tomorrow, (dialog, which) -> OptionsControl
+                            .setString(R.string.prefid_font_remind_date, LocalDate.now().toString()));
+                    dialogBuilder.setNeutralButton(R.string.ignore,
+                            (dialog, which) -> OptionsControl.setBoolean(R.string.prefid_ignore_font_provider, true));
+                    dialogBuilder.show();
+                }
+            }
+        }
+        else if (code == FontProviderClient.FontProviderAvailability.VERSION_TOO_LOW)
+        {
+            LocalDate remindDate = OptionsControl.getDate(R.string.prefid_font_update_alert_date);
+            if ((remindDate == null) || remindDate.isBefore(LocalDate.now()))
+            {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+                dialogBuilder.setMessage(R.string.font_provider_update_request);
+                dialogBuilder.setPositiveButton(R.string.update_on_google_play, (dialog, which) ->
+                {
+                    activity.startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://play.google.com/store/apps/details?id=moe.shizuku.fontprovider")));
+//                    activity.startActivity(new Intent(Intent.ACTION_VIEW,
+//                            Uri.parse("https://github.com/RikkaApps/FontProvider/releases/")));
+                });
+                dialogBuilder.setNegativeButton(R.string.remind_tomorrow, (dialog, which) -> OptionsControl
+                        .setString(R.string.prefid_font_update_alert_date, LocalDate.now().toString()));
+                dialogBuilder.show();
+            }
+        }
+        else if ((code == FontProviderClient.FontProviderAvailability.OK) && !isFontInitialized)
         {
             FontProviderClient client = FontProviderClient.create(activity);
             if (client != null)
