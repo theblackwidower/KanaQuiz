d9269b2f66c9ede6cb95f684f27cbf4bd25af0df
T Duke Perry <theblackwidower@noprestige.com>
2019-08-18 00:48:16 -0400

    Wrote migration code
    Wrote the necessary code for migrating users to the new database design without losing data. Unlike previous migrations, I could not use a single command to manage the transfer, simply because part of this migration involves actually sorting which 'type' category each question belongs in from just looking at the question text. So I actually have to loop through the items and analyse each one individually in Java code to determine which 'type' the question belongs to. Because of this, I had to look back at some old code (30c55cc1394a3a3977a9ba05b571e1fe8cfed5d2) from back when the migrations were written to loop through the items individually, before I rewrote them to handle all that in SQL code (a960ccaef37e9097a6ebea29b3a8cbfa2b50e88d). I copied some of the old code and tweaked it to fit the new migration. As part of this, I also wrote a new method that is designed exclusively to determine which type of question we're looking at from just the text. I needed to do this because it's being used for two different tables in two distinct sections of the migration, and I don't want repeating code. The whole thing still needs to be tested, but it should work.

--- app/src/main/java/com/noprestige/kanaquiz/logs/LogDatabase.java
+++ app/src/main/java/com/noprestige/kanaquiz/logs/LogDatabase.java
@@ -17,6 +17,9 @@
 package com.noprestige.kanaquiz.logs;
 
 import android.app.Application;
+import android.database.Cursor;
+
+import com.noprestige.kanaquiz.questions.QuestionType;
 
 import org.threeten.bp.LocalDate;
 
@@ -39,7 +42,7 @@ public abstract class LogDatabase extends RoomDatabase
     {
         if (DAO == null)
             DAO = Room.databaseBuilder(context, LogDatabase.class, "user-logs")
-                    .addMigrations(MIGRATION_1_2, MIGRATION_2_3).build().logDao();
+                    .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4).build().logDao();
     }
 
     public static final Migration MIGRATION_1_2 = new Migration(1, 2)
@@ -82,6 +85,109 @@ public abstract class LogDatabase extends RoomDatabase
                             "incorrect_romanji))");
             database.execSQL("INSERT INTO incorrect_answers (date, kana, incorrect_romanji, occurrences) SELECT '" +
                     currentDate + "', kana, incorrect_romanji, occurrences FROM old_incorrect_answers");
+            database.execSQL("DROP TABLE old_incorrect_answers");
+        }
+    };
+
+    private static final QuestionType identifyQuestion(String question)
+    {
+        int kanaCount = 0;
+        int kanjiCount = 0;
+        int latinCount = 0;
+        for (int i = 0; i < question.length(); i++)
+        {
+            if ((question.charAt(i) >= 0x3040) && (question.charAt(i) <= 0x30FF))
+                kanaCount++;
+            else if (((question.charAt(i) >= 0x3400) && (question.charAt(i) <= 0x4DB5)) ||
+                    ((question.charAt(i) >= 0x4E00) && (question.charAt(i) <= 0x9FCB)) ||
+                    ((question.charAt(i) >= 0xF900) && (question.charAt(i) <= 0xFA6A)))
+                kanjiCount++;
+            else if (((question.charAt(i) >= 0x0041) && (question.charAt(i) <= 0x005A)) ||
+                    ((question.charAt(i) >= 0x0061) && (question.charAt(i) <= 0x007A)))
+                latinCount++;
+        }
+
+        if ((kanaCount > 0) && (kanjiCount == 0) && (latinCount == 0))
+            return QuestionType.KANA;
+        else if ((kanaCount == 0) && (kanjiCount > 0) && (latinCount == 0))
+            return QuestionType.KANJI;
+        else
+            return QuestionType.VOCABULARY;
+    }
+
+    public static final Migration MIGRATION_3_4 = new Migration(3, 4)
+    {
+        @Override
+        public void migrate(SupportSQLiteDatabase database)
+        {
+            //rebuilding tables because ALTER TABLE can't alter, or even add to, the primary key
+            database.execSQL(
+                    "CREATE TABLE IF NOT EXISTS question_records (date INTEGER NOT NULL, question TEXT NOT NULL, type" +
+                            " INTEGER NOT NULL, correct_answers INTEGER NOT NULL, incorrect_answers INTEGER NOT NULL," +
+                            " PRIMARY KEY(date, question, type))");
+
+            Cursor kanaData = database.query("SELECT * FROM kana_records");
+
+            if (kanaData.getCount() > 0)
+            {
+                int dateIndex = kanaData.getColumnIndex("date");
+                int questionIndex = kanaData.getColumnIndex("kana");
+                int correctIndex = kanaData.getColumnIndex("correct_answers");
+                int incorrectIndex = kanaData.getColumnIndex("incorrect_answers");
+
+                while (!kanaData.isLast())
+                {
+                    kanaData.moveToNext();
+
+                    int dateValue = kanaData.getInt(dateIndex);
+                    String questionValue = kanaData.getString(questionIndex);
+                    int correctValue = kanaData.getInt(correctIndex);
+                    int incorrectValue = kanaData.getInt(incorrectIndex);
+
+                    char typeId = LogTypeConversion.toCharFromType(identifyQuestion(questionValue));
+
+                    database.execSQL(
+                            "INSERT INTO question_records (date, question, type, correct_answers, incorrect_answers) " +
+                                    "VALUES (?, ?, ?, ?, ?)",
+                            new Object[]{dateValue, questionValue, typeId, correctValue, incorrectValue});
+                }
+            }
+            database.execSQL("DROP TABLE kana_records");
+
+
+            database.execSQL("ALTER TABLE incorrect_answers RENAME TO old_incorrect_answers");
+
+            database.execSQL(
+                    "CREATE TABLE IF NOT EXISTS incorrect_answers (date INTEGER NOT NULL, question TEXT NOT NULL, " +
+                            "type INTEGER NOT NULL, incorrect_answer TEXT NOT NULL, occurances INTEGER NOT NULL, " +
+                            "PRIMARY KEY(date, question, type, incorrect_answer))");
+
+            Cursor incorrectData = database.query("SELECT * FROM old_incorrect_answers");
+
+            if (incorrectData.getCount() > 0)
+            {
+                int dateIndex = incorrectData.getColumnIndex("date");
+                int questionIndex = incorrectData.getColumnIndex("kana");
+                int answerIndex = incorrectData.getColumnIndex("incorrect_romanji");
+                int occurrencesIndex = incorrectData.getColumnIndex("occurrences");
+
+                while (!incorrectData.isLast())
+                {
+                    incorrectData.moveToNext();
+
+                    int dateValue = incorrectData.getInt(dateIndex);
+                    String questionValue = incorrectData.getString(questionIndex);
+                    String answerValue = incorrectData.getString(answerIndex);
+                    int occurrencesValue = incorrectData.getInt(occurrencesIndex);
+
+                    char typeId = LogTypeConversion.toCharFromType(identifyQuestion(questionValue));
+
+                    database.execSQL(
+                            "INSERT INTO incorrect_answers (date, question, type, incorrect_answer, occurrences) " +
+                                    "VALUES (?, ?, ?, ?, ?)",
+                            new Object[]{dateValue, questionValue, typeId, answerValue, occurrencesValue});
+                }
+            }
             database.execSQL("DROP TABLE old_incorrect_answers");
         }
     };
