6f9231290c735200f38221c7db9c823756e48c0a
T Duke Perry <theblackwidower@noprestige.com>
2021-05-24 12:03:15 -0400

    Replacing resource-filled switch case
    Because resource ids are apparently going to be made non-final in the next AGP (Android Gradle Plugin) update, and it's a bad idea to use variables in switch-cases, I decided to quickly convert it into an if-else chain. I assume that's better.

--- app/src/main/java/com/noprestige/kanaquiz/quiz/MainQuiz.java
+++ app/src/main/java/com/noprestige/kanaquiz/quiz/MainQuiz.java
@@ -298,28 +298,25 @@ public class MainQuiz extends AppCompatActivity
         Class destination;
         int result = 0;
 
-        switch (item.getItemId())
+        int id = item.getItemId();
+        if (id == R.id.mnuSelection)
         {
-            case R.id.mnuSelection:
-                destination = QuestionSelection.class;
-                result = 1;
-                break;
-            case R.id.mnuReference:
-                destination = ReferenceScreen.class;
-                break;
-            case R.id.mnuOptions:
-                destination = OptionsScreen.class;
-                result = 1;
-                break;
-            case R.id.mnuLogs:
-                destination = LogView.class;
-                break;
-            case R.id.mnuAbout:
-                destination = AboutScreen.class;
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
+            destination = QuestionSelection.class;
+            result = 1;
         }
+        else if (id == R.id.mnuReference)
+            destination = ReferenceScreen.class;
+        else if (id == R.id.mnuOptions)
+        {
+            destination = OptionsScreen.class;
+            result = 1;
+        }
+        else if (id == R.id.mnuLogs)
+            destination = LogView.class;
+        else if (id == R.id.mnuAbout)
+            destination = AboutScreen.class;
+        else
+            return super.onOptionsItemSelected(item);
 
         startActivityForResult(new Intent(this, destination), result);
         return true;
